997bb012de67d332fe768720b480356a02a196fc
Vrishali34 <jvrishali1@gmail.com>
2026-06-26 15:12:43 +0530

    Add CurrentAccount with overdraft and method overriding via _adjustBalance helper

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ class Account {
     Account.totalAccounts++;
   }
 
+  //overriding method
+  _adjustBalance(amount){
+    this.#balance += amount; //both positive(deposit) and negative(withdraw) 
+  }
+
   deposit(amount) {
     if (amount <= 0) {
       console.log("Deposit amount must be greater than 0");
@@ -55,6 +60,26 @@ class SavingsAccount extends Account{
   }
 }
 
+class CurrentAccount extends Account{
+  constructor(accountHolder,accountNumber,initialBalance,overdraftLimit){
+    super(accountHolder,accountNumber,initialBalance);
+    this.overdraftLimit = overdraftLimit;
+  }
+  withdraw(amount){
+    if (amount <= 0){
+      console.log("Withdrawal amount must be greater than 0");
+      return;
+    }
+    if(this.balance - amount < -this.overdraftLimit){
+      console.log("Overdraft limit exceeded");
+      return;
+    }
+    this._adjustBalance(-amount);
+    console.log(`Withdrew ${amount}. New balance: ${this.balance}`);
+  }
+    
+}
+
 
 // testing 1 - for createing accoint , reflecting deposit, withdrqw , balance
 const acc1=new Account("Vrishali","ACC001",1000);
@@ -65,15 +90,20 @@ acc1.withdraw(5000);
 console.log(acc1.balance);
 
 //testing 2 - for to count total no of accounts
-
 const acc2=new Account("Ram","ACC002",1000);
 const acc3=new Account("Sham","ACC003",2000);
 console.log(Account.getTotalAccounts());
 
 //testing 3
-
-
 const sav1 = new SavingsAccount("Raju", "SAV001", 1000, 0.05); // 5% interest
 sav1.addInterest(); // Deposited 50. New balance: 1050  →  Interest of 50 added.
 console.log(sav1.balance); // 1050
+
+//testing 4
+const curr1 = new CurrentAccount("Vrishali","CUR001",1000,500);
+curr1.withdraw(1200);
+console.log(curr1.balance);
+
+curr1.withdraw(500);
+console.log(curr1.balance);
 
